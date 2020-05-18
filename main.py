fc39c70f5cc56c6a298bf89e1771b61b6808c4f3
wertousek <42165903+wertousek@users.noreply.github.com>
2020-05-18 17:48:01 +0200

    trh

--- main.py
+++ main.py
@@ -84,5 +84,7 @@ async def on_message(message):
 			team = choice(teams.read().split("\n"))
 			print(team)
 			await message.channel.send(team)
+	if "trh" == commandos:
+		print "domácí trh se nejspíše aktualizuje po těchto kolech: 3,5,8,10,13,15,18,20,23,25,28,30,33,35,38,40,43,45, s tím, že každý druhé jmenované kolo se nejspíše aktualizuje i světový trh"
 
 client.run(token)
