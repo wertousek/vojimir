c9d72ba136088bf2cdbb8381d7cd87d922fc561d
wertousek <42165903+wertousek@users.noreply.github.com>
2020-11-09 21:37:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,8 @@ import json
 token = os.environ.get('TOKEN', None)
 
 
-client = bdbf.Client(commandPrefix = "-", embedFooter = {"text": "Powered by wertousek","icon_url":"https://cdn.discordapp.com/avatars/436131686640648195/d72e4885e1d21bca46bd245bb00c4687.png"}
+client = bdbf.Client(commandPrefix = "-", embedFooter = {"text": "Powered by wertousek","icon_url":"https://cdn.discordapp.com/avatars/436131686640648195/d72e4885e1d21bca46bd245bb00c4687.png"})
 guild = client.get_guild(710900407639081002)
-
-bdbf.commandPrefix = "-"
-bdbf.embedFooter = {
-                "text": "Powered by wertousek",
-                "icon_url": "https://cdn.discordapp.com/avatars/436131686640648195/d72e4885e1d21bca46bd245bb00c4687.png"
-                }
 
 with open ("sprostySlovnik.json","r") as sprostySlovnik:
 	sprostaSlova = json.loads(sprostySlovnik.read())
